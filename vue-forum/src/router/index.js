af3df03ae967f7205d9c22177d45bd0874bda539
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-29 16:44:21 +0200

    Redirect With Vue Router Without Changing the URL

--- vue-forum/src/router/index.js
+++ vue-forum/src/router/index.js
@@ -1,24 +1,40 @@
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import { createRouter, createWebHistory } from 'vue-router'
-import PageNotFound from "@/components/PageNotFound"
+import PageHome from "@/components/PageHome";
+import PageThreadShow from "@/components/PageThreadShow";
+import { createRouter, createWebHistory } from "vue-router";
+import PageNotFound from "@/components/PageNotFound";
+import sourceData from "@/data.json";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: PageHome
+    path: "/",
+    name: "Home",
+    component: PageHome,
   },
   {
-    path: '/thread/:id',
-    name: 'ThreadShow',
+    path: "/thread/:id",
+    name: "ThreadShow",
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter(to, from, next) {
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+      if (threadExists) {
+        return next();
+      } else {
+        next({
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
   },
-  {path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound}
-]
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: PageNotFound },
+];
 
 export default createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
