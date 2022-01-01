e5644db158c8286a4cf175615b4983a0c1d1fe26
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-01-01 14:28:06 +0200

    Communication Between Components

--- vue-forum/src/pages/ThreadShow.vue
+++ vue-forum/src/pages/ThreadShow.vue
@@ -2,34 +2,19 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="threadPosts" />
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea
-            v-model="newPostText"
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            class="form-input"
-          >
-          </textarea>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">Submit post</button>
-        </div>
-      </form>
-    </div>
+    <PostEditor @save="addPost" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data.json";
 import PostList from "@/components/PostList";
+import PostEditor from "@/components/PostEditor";
 export default {
   name: "ThreadShow",
   components: {
     PostList,
+    PostEditor,
   },
   props: {
     id: {
@@ -41,7 +26,6 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
-      newPostText: "",
     };
   },
   computed: {
@@ -53,18 +37,13 @@ export default {
     },
   },
   methods: {
-    addPost() {
-      const postId = "qqqq" + Math.random();
+    addPost(eventData) {
       const post = {
-        id: postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
+        ...eventData.post,
         threadId: this.id,
-        userId: "VXjpr2WHa8Ux4Bnggym8QFLdv5C3",
       };
       this.posts.push(post);
-      this.thread.posts.push(postId);
-      this.newPostText = "";
+      this.thread.posts.push(post.id);
     },
   },
 };
