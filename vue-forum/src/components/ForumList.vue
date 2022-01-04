2062800c4844536be215514ea769d0fd2e61bf36
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-01-04 21:52:37 +0200

    Creating a ForumList Component

---
+++ vue-forum/src/components/ForumList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="col-full">
+    <div class="forum-list">
+      <h2 class="list-title">
+        <a href="#">Forums</a>
+      </h2>
+
+      <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link :to="{ name: 'Forum', params: { id: forum.id } }">
+            {{ forum.name }}
+          </router-link>
+          <p>{{ forum.description }}</p>
+        </div>
+
+        <div class="threads-count">
+          <p>
+            <span class="count">{{ forum.threads?.length }}</span>
+            {{ forumThreadsWord(forum) }}
+          </p>
+        </div>
+
+        <div class="last-thread"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forums: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    forumThreadsWord(forum) {
+      if (forum.threads?.length) {
+        return forum.threads.length > 1 ? "threads" : "thread";
+      } else {
+        return "no threads";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
