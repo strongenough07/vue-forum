dc12c29fff0fa7e40d92e21d1ec917f8054a59e7
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-01-23 16:19:03 +0200

    Refactoring the Forum Thread Related Actions and Mutations

--- vue-forum/src/store/index.js
+++ vue-forum/src/store/index.js
@@ -35,6 +35,22 @@ export default createStore({
         threadId: post.threadId,
       });
     },
+    createThread({ commit, state, dispatch }, { text, title, forumId }) {
+      const id = "qqqq" + Math.random();
+      const userId = state.authId;
+      const publishedAt = Math.floor(Date.now() / 1000);
+      const thread = {
+        forumId,
+        title,
+        publishedAt,
+        userId,
+        id,
+      };
+      commit("setThread", { thread });
+      commit("appendThreadToUser", { userId, threadId: id });
+      commit("appendThreadToForum", { forumId, threadId: id });
+      dispatch("createPost", { text, threadId: id });
+    },
     updateUser({ commit }, user) {
       commit("setUser", { user, userId: user.id });
     },
@@ -43,13 +59,27 @@ export default createStore({
     setPost(state, { post }) {
       state.posts.push(post);
     },
+    setThreads(state, { thread }) {
+      state.threads.push(thread);
+    },
     setUser(state, { user, userId }) {
       const userIndex = state.users.findIndex((user) => user.id === userId);
       state.users[userIndex] = user;
     },
     appendPostToThread(state, { postId, threadId }) {
       const thread = state.threads.find((thread) => thread.id === threadId);
+      thread.posts = thread.posts || [];
       thread.posts.push(postId);
+    },
+    appendThreadToForum(state, { forumId, threadId }) {
+      const forum = state.threads.find((forum) => forum.id === forumId);
+      forum.threads = forum.threads || [];
+      forum.threads.push(threadId);
+    },
+    appendThreadToUser(state, { userId, threadId }) {
+      const user = state.threads.find((user) => user.id === userId);
+      user.threads = user.threads || [];
+      user.threads.push(threadId);
     },
   },
 });
