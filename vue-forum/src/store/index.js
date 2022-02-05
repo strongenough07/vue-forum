cd5418202d9943f911d01b51622b7ebe585f60b7
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-02-05 13:26:00 +0200

    Creating Vuex Mutations with Higher Order Functions

--- vue-forum/src/store/index.js
+++ vue-forum/src/store/index.js
@@ -32,8 +32,8 @@ export default createStore({
       (post.publishedAt = Math.floor(Date.now() / 1000)),
         commit("setPost", { post });
       commit("appendPostToThread", {
-        postId: post.id,
-        threadId: post.threadId,
+        childId: post.id,
+        parentId: post.threadId,
       });
     },
     async createThread({ commit, state, dispatch }, { text, title, forumId }) {
@@ -48,8 +48,8 @@ export default createStore({
         id,
       };
       commit("setThread", { thread });
-      commit("appendThreadToUser", { userId, threadId: id });
-      commit("appendThreadToForum", { forumId, threadId: id });
+      commit("appendThreadToUser", { parentId: userId, childId: id });
+      commit("appendThreadToForum", { parentId: forumId, childId: id });
       dispatch("createPost", { text, threadId: id });
       return findById(state.threads, id);
     },
@@ -77,20 +77,25 @@ export default createStore({
       const userIndex = state.users.findIndex((user) => user.id === userId);
       state.users[userIndex] = user;
     },
-    appendPostToThread(state, { postId, threadId }) {
-      const thread = findById(state.threads, threadId);
-      thread.posts = thread.posts || [];
-      thread.posts.push(postId);
-    },
-    appendThreadToForum(state, { forumId, threadId }) {
-      const forum = findById(state.forums, forumId);
-      forum.threads = forum.threads || [];
-      forum.threads.push(threadId);
-    },
-    appendThreadToUser(state, { userId, threadId }) {
-      const user = findById(state.users, userId);
-      user.threads = user.threads || [];
-      user.threads.push(threadId);
-    },
+    appendPostToThread: makeAppendChildToParentMutation({
+      parent: "threads",
+      child: "posts",
+    }),
+    appendThreadToForum: makeAppendChildToParentMutation({
+      parent: "forums",
+      child: "threads",
+    }),
+    appendThreadToUser: makeAppendChildToParentMutation({
+      parent: "users",
+      child: "threads",
+    }),
   },
 });
+
+function makeAppendChildToParentMutation({ parent, child }) {
+  return (state, { childId, parentId }) => {
+    const resource = findById(state[parent], parentId);
+    resource[child] = resource[child] || [];
+    resource[child].push(childId);
+  };
+}
