e37bc9e6db3517a6a6dbddbd36517c6bf6f64e46
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-02-12 14:41:47 +0200

    Creating Vuex Actions that Fetch Multiple Items from Cloud Firestore

--- vue-forum/src/pages/ThreadShow.vue
+++ vue-forum/src/pages/ThreadShow.vue
@@ -74,10 +74,11 @@ export default {
     this.$store.dispatch("fetchUser", { id: thread.userId });
 
     // fetch the posts
-    thread.posts.forEach(async (postId) => {
-      const post = await this.$store.dispatch("fetchPost", { id: postId });
-      this.$store.dispatch("fetchUser", { id: post.userId });
+    const posts = await this.$store.dispatch("fetchPosts", {
+      ids: thread.posts,
     });
+    const users = posts.map((post) => post.userId);
+    this.$store.dispatch("fetchUsers", { ids: users });
   },
 };
 </script>
