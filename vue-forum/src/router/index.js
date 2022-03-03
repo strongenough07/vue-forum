9f3f20032286835eef6bdb85b45d9ffdfea5bce5
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-03 19:28:04 +0200

    Firestore Rules and Protecting Pages for Authenticated Users

--- vue-forum/src/router/index.js
+++ vue-forum/src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
     name: "ProfileEdit",
     component: Profile,
     props: { edit: true },
-    
+    meta: {requiresAuth: true}
   },
   {
     path: "/category/:id",
@@ -72,13 +72,15 @@ const routes = [
     path: "/forum/:forumId/thread/create",
     name: "ThreadCreate",
     component: ThreadCreate,
-    props: true
+    props: true,
+    meta: {requiresAuth: true}
   },
   {
     path: "/thread/:id/edit",
     name: "ThreadEdit",
     component: ThreadEdit,
-    props: true
+    props: true,
+    meta: {requiresAuth: true}
   },
   {
     path: "/register",
