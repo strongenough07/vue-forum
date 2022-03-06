e8093d680096cc1b4990484c6d2ffb4a586c54d1
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-06 14:32:41 +0200

    Splitting a Large Vuex Store Into Namespaced Modules Part 2

--- vue-forum/src/store/mutations.js
+++ vue-forum/src/store/mutations.js
@@ -1,7 +1,7 @@
 import { upsert, findById, docToResource } from '@/helpers'
 export default {
   setItem (state, { resource, item }) {
-    upsert(state[resource], docToResource(item))
+    upsert(state[resource].items, docToResource(item))
   },
 
   appendUnsubscribe (state, { unsubscribe }) {
