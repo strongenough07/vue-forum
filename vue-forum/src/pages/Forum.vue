427c44309a889f0a70118f624bb1d270e5f2359e
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-13 10:54:02 +0200

    Paginating Threads with Pagination Component

--- vue-forum/src/pages/Forum.vue
+++ vue-forum/src/pages/Forum.vue
@@ -1,31 +1,37 @@
 <template>
-<div v-if="asyncDataStatus_ready" class="col-full">
-  <div v-if="forum" class="col-full push-top">
-    <div class="forum-header">
-      <div class="forum-details">
-        <h1>{{ forum.name }}</h1>
-        <p class="text-lead">{{ forum.description }}</p>
+  <div v-if="asyncDataStatus_ready" class="col-full">
+    <div v-if="forum" class="col-full push-top">
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>{{ forum.name }}</h1>
+          <p class="text-lead">{{ forum.description }}</p>
+        </div>
+        <router-link
+          :to="{ name: 'ThreadCreate', params: { forumId: forum.id } }"
+          class="btn-green btn-small"
+        >
+          Start a thread
+        </router-link>
       </div>
-      <router-link
-        :to="{name:'ThreadCreate', params: {forumId: forum.id}}"
-        class="btn-green btn-small"
-      >
-        Start a thread
-      </router-link>
+    </div>
+
+    <div class="col-full push-top">
+      <ThreadList :threads="threads" />
+      <v-pagination
+        v-model="page"
+        :pages="totalPages"
+        active-color="#DCEDFF"
+        @update:modelValue="updateHandler"
+      />
     </div>
   </div>
-
-  <div class="col-full push-top">
-    <ThreadList :threads="threads"/>
-  </div>
-</div>
 </template>
 
 <script>
-import ThreadList from '@/components/ThreadList'
-import { findById } from '@/helpers'
-import { mapActions } from 'vuex'
-import asyncDataStatus from '@/mixins/asyncDataStatus'
+import ThreadList from "@/components/ThreadList";
+import { findById } from "@/helpers";
+import { mapActions } from "vuex";
+import asyncDataStatus from "@/mixins/asyncDataStatus";
 export default {
   components: { ThreadList },
   mixins: [asyncDataStatus],
@@ -36,28 +42,55 @@ export default {
     }
   },
   computed: {
-    forum () {
-      return findById(this.$store.state.forums.items, this.id)
+    forum() {
+      return findById(this.$store.state.forums.items, this.id);
     },
-    threads () {
-      if (!this.forum) return []
-      return this.forum.threads.map(threadId => this.$store.getters['threads/thread'](threadId))
+    threads() {
+      if (!this.forum) return [];
+      return this.$store.state.threads.items
+        .filter(thread => thread.forumId === this.forum.id)
+        .map(thread => this.$store.getters["threads/thread"](thread.id));
+    },
+    threadCount() {
+      return this.forum.threads.length;
+    },
+    totalPages() {
+      if (!this.threadCount) return 0;
+      return Math.ceil(this.threadCount / this.perPage);
     }
   },
-  methods: {
-    ...mapActions('forums', ['fetchForum']),
-    ...mapActions('threads', ['fetchThreads']),
-    ...mapActions('users', ['fetchUsers'])
+  data() {
+    return {
+      page: 1,
+      perPage: 10
+    };
   },
-  async created () {
-    const forum = await this.fetchForum({ id: this.id })
-    const threads = await this.fetchThreads({ ids: forum.threads })
-    await this.fetchUsers({ ids: threads.map(thread => thread.userId) })
-    this.asyncDataStatus_fetched()
+  methods: {
+    ...mapActions("forums", ["fetchForum"]),
+    ...mapActions("threads", ["fetchThreadsByPage"]),
+    ...mapActions("users", ["fetchUsers"])
+  },
+  async created() {
+    const forum = await this.fetchForum({ id: this.id });
+    const threads = await this.fetchThreadsByPage({
+      ids: forum.threads,
+      page: this.page,
+      perPage: this.perPage
+    });
+    await this.fetchUsers({ ids: threads.map(thread => thread.userId) });
+    this.asyncDataStatus_fetched();
+  },
+  watch: {
+    async page(page) {
+      const threads = await this.fetchThreadsByPage({
+        ids: this.forum.threads,
+        page: this.page,
+        perPage: this.perPage
+      });
+      await this.fetchUsers({ ids: threads.map(thread => thread.userId) });
+    }
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
