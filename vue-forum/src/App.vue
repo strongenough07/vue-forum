5af6e8d9db1674e43610a92f8edc35df95c87f7c
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-13 12:22:00 +0200

    Storing the Current Page as a URL Query Variable

--- vue-forum/src/App.vue
+++ vue-forum/src/App.vue
@@ -1,48 +1,52 @@
 <template>
-  <the-navbar/>
+  <the-navbar />
   <div class="container">
-    <router-view v-show="showPage" @ready="onPageReady" :key="$route.path"/>
+    <router-view
+      v-show="showPage"
+      @ready="onPageReady"
+      :key="`${$route.path}${JSON.stringify($route.query)}`"
+    />
     <AppSpinner v-show="!showPage" />
   </div>
 </template>
 
 <script>
-import TheNavbar from '@/components/TheNavbar'
-import { mapActions } from 'vuex'
-import NProgress from 'nprogress'
+import TheNavbar from "@/components/TheNavbar";
+import { mapActions } from "vuex";
+import NProgress from "nprogress";
 export default {
-  name: 'App',
+  name: "App",
   components: { TheNavbar },
-  data () {
+  data() {
     return {
       showPage: false
-    }
+    };
   },
   methods: {
-    ...mapActions('auth', ['fetchAuthUser']),
-    onPageReady () {
-      this.showPage = true
-      NProgress.done()
+    ...mapActions("auth", ["fetchAuthUser"]),
+    onPageReady() {
+      this.showPage = true;
+      NProgress.done();
     }
   },
-  created () {
-    this.fetchAuthUser()
+  created() {
+    this.fetchAuthUser();
     NProgress.configure({
       speed: 200,
       showSpinner: false
-    })
+    });
     this.$router.beforeEach(() => {
-      this.showPage = false
-      NProgress.start()
-    })
+      this.showPage = false;
+      NProgress.start();
+    });
   }
-}
+};
 </script>
 
 <style>
 @import "assets/style.css";
 @import "~nprogress/nprogress.css";
-#nprogress .bar{
-  background: #57AD8D !important;
+#nprogress .bar {
+  background: #57ad8d !important;
 }
 </style>
