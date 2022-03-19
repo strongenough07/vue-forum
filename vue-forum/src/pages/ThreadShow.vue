8eecb5b7af881506fe90c2e28ddaac09907ad133
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-19 13:21:23 +0200

    Implementing the Notifications on Thread Page Part 2

--- vue-forum/src/pages/ThreadShow.vue
+++ vue-forum/src/pages/ThreadShow.vue
@@ -12,11 +12,13 @@
       </router-link>
     </h1>
     <p>
-      By <a href="#" class="link-unstyled">{{thread.author?.name}}</a>, <AppDate :timestamp="thread.publishedAt" />.
+      By <a href="#" class="link-unstyled">{{ thread.author?.name }}</a
+      >, <AppDate :timestamp="thread.publishedAt" />.
       <span
         style="float:right; margin-top: 2px;"
         class="hide-mobile text-faded text-small"
-        >{{thread.repliesCount}} replies by {{thread.contributorsCount}} contributors</span
+        >{{ thread.repliesCount }} replies by
+        {{ thread.contributorsCount }} contributors</span
       >
     </p>
 
@@ -24,20 +26,28 @@
 
     <post-editor v-if="authUser" @save="addPost" />
     <div v-else class="text-center" style="margin-bottom: 50px;">
-      <router-link :to="{name: 'SignIn', query:{redirectTo: $route.path}}">Sign In</router-link> or <router-link :to="{name: 'Register',  query:{redirectTo: $route.path}}">Register</router-link> to reply.
+      <router-link :to="{ name: 'SignIn', query: { redirectTo: $route.path } }"
+        >Sign In</router-link
+      >
+      or
+      <router-link
+        :to="{ name: 'Register', query: { redirectTo: $route.path } }"
+        >Register</router-link
+      >
+      to reply.
     </div>
   </div>
 </template>
 
 <script>
-import PostList from '@/components/PostList'
-import PostEditor from '@/components/PostEditor'
-import { mapActions, mapGetters } from 'vuex'
-import asyncDataStatus from '@/mixins/asyncDataStatus'
-import useNotifications from '@/composables/useNotifications'
-import difference from 'lodash/difference'
+import PostList from "@/components/PostList";
+import PostEditor from "@/components/PostEditor";
+import { mapActions, mapGetters } from "vuex";
+import asyncDataStatus from "@/mixins/asyncDataStatus";
+import useNotifications from "@/composables/useNotifications";
+import difference from "lodash/difference";
 export default {
-  name: 'ThreadShow',
+  name: "ThreadShow",
   components: {
     PostList,
     PostEditor
@@ -49,57 +59,73 @@ export default {
       type: String
     }
   },
-  setup () {
-    const { addNotification } = useNotifications()
-    return { addNotification }
+  setup() {
+    const { addNotification } = useNotifications();
+    return { addNotification };
   },
   computed: {
-    ...mapGetters('auth', ['authUser']),
-    threads () {
-      return this.$store.state.threads.items
+    ...mapGetters("auth", ["authUser"]),
+    threads() {
+      return this.$store.state.threads.items;
     },
-    posts () {
-      return this.$store.state.posts.items
+    posts() {
+      return this.$store.state.posts.items;
     },
-    thread () {
-      return this.$store.getters['threads/thread'](this.id)
+    thread() {
+      return this.$store.getters["threads/thread"](this.id);
     },
-    threadPosts () {
-      return this.posts.filter(post => post.threadId === this.id)
+    threadPosts() {
+      return this.posts.filter(post => post.threadId === this.id);
     }
   },
   methods: {
-    ...mapActions('threads', ['fetchThread']),
-    ...mapActions('users', ['fetchUsers']),
-    ...mapActions('posts', ['fetchPosts', 'createPost']),
-    async fetchPostsWithUsers (ids) {
+    ...mapActions("threads", ["fetchThread"]),
+    ...mapActions("users", ["fetchUsers"]),
+    ...mapActions("posts", ["fetchPosts", "createPost"]),
+    async fetchPostsWithUsers(ids) {
       // fetch the posts
-      const posts = await this.fetchPosts({ ids })
+      const posts = await this.fetchPosts({
+        ids,
+        onSnapshot: ({ isLocal, previousItem }) => {
+          console.log(isLocal);
+          if (
+            !this.asyncDataStatus_ready ||
+            isLocal ||
+            (previousItem?.edited && !previousItem?.edited?.at)
+          )
+            return;
+          this.addNotification({ message: "Thread recently updated" });
+        }
+      });
       // fetch the users associated with the posts
-      const users = posts.map(post => post.userId).concat(this.thread.userId)
-      await this.fetchUsers({ ids: users })
+      const users = posts.map(post => post.userId).concat(this.thread.userId);
+      await this.fetchUsers({ ids: users });
     },
-    addPost (eventData) {
+    addPost(eventData) {
       const post = {
         ...eventData.post,
         threadId: this.id
-      }
-      this.createPost(post)
+      };
+      this.createPost(post);
     }
   },
-  async created () {
+  async created() {
     // fetch the thread
     const thread = await this.fetchThread({
       id: this.id,
       onSnapshot: async ({ isLocal, item, previousItem }) => {
-        if (!this.asyncDataStatus_ready || isLocal) return
-        const newPosts = difference(item.posts, previousItem.posts)
-        await this.fetchPostsWithUsers(newPosts)
-        this.addNotification({ message: 'Thread recently updated' })
+        if (!this.asyncDataStatus_ready || isLocal) return;
+        const newPosts = difference(item.posts, previousItem.posts);
+        const hasNewPosts = newPosts.length > 0;
+        if (hasNewPosts) {
+          await this.fetchPostsWithUsers(newPosts);
+        } else {
+          this.addNotification({ message: "Thread recently updated" });
+        }
       }
-    })
-    await this.fetchPostsWithUsers(thread.posts)
-    this.asyncDataStatus_fetched()
+    });
+    await this.fetchPostsWithUsers(thread.posts);
+    this.asyncDataStatus_fetched();
   }
-}
+};
 </script>
