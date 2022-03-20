0a12838ad27cf785a90a78f8e392daeef69ea8c2
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-20 21:31:42 +0200

    Storing Images to Firebase Storage from a Web URL

--- vue-forum/src/components/UserProfileCardEditor.vue
+++ vue-forum/src/components/UserProfileCardEditor.vue
@@ -128,7 +128,21 @@ export default {
       this.activeUser.avatar = uploadedImage || this.activeUser.avatar;
       this.uploadingImage = false;
     },
-    save() {
+    async handleRandomAvatarUpload() {
+      const randomAvatarGenerated = await this.activeUser.avatar.startsWith(
+        "https://pixabay"
+      );
+      if (randomAvatarGenerated) {
+        const image = await fetch(this.activeUser.avatar);
+        const blob = await image.blob();
+        this.activeUser.avatar = await this.uploadAvatar({
+          file: blob,
+          filename: "random"
+        });
+      }
+    },
+    async save() {
+      await this.handleRandomAvatarUpload();
       this.$store.dispatch("users/updateUser", { ...this.activeUser });
       this.$router.push({ name: "Profile" });
     },
